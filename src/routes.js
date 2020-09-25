5dec5e961916a62abc8d2b705f8f6facf2f5011a
marcio <marcio.ferreira@cialne.com.br>
2020-09-25 15:38:39 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcio Tadeu',
+    email: 'marcio.tadeu2@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
