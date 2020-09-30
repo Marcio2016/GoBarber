7004424d678daf10c0cd9cc72a70b264a34ed8a5
marcio <marcio.tadeu2@gmail.com>
2020-09-30 19:11:34 -0300

    Middleware de autenticação e Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,6 +15,29 @@ class UserController {
       provider,
     });
   }
+  async update(req,res) {
+    const {email,oldpassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    if(email && email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+      if (userExist) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if(oldpassword && !(await user.checkPassword(oldpassword))) {
+      return res.status(401).json({error: 'Password does not match'})
+    }
+
+    const {id,name,provider} = user.update(req.body);
+    
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
