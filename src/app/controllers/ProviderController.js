a1c30283072816ac1632948a3087fa0efab72eac
marcio <marcio.ferreira@cialne.com.br>
2020-10-20 12:58:42 -0300

    Lisatando prestadories de serviços

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,21 @@
+import user from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await user.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
