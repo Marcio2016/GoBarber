ef43f55c30085afe0a5bbd9d1f814df58e4a2973
marcio <marcio.ferreira@cialne.com.br>
2020-10-22 13:25:16 -0300

    Aplicando paginação

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,31 +1,32 @@
-import Appointment from '../models/Appointments'
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import * as YUP from 'yup';
+import Appointment from '../models/Appointments';
 import User from '../models/User';
 import File from '../models/File';
-import {startOfHour,parseISO,isBefore} from 'date-fns';
-import * as YUP from 'yup';
-
 
 class AppointmentController {
-  async store(req,res ) {
+  async store(req, res) {
     const schema = YUP.object().shape({
       provider_id: YUP.number().required(),
-      date: YUP.date().required()
+      date: YUP.date().required(),
     });
 
-    if(!(await schema.isValid(req.body))){
-      return res.status(400).json({error: 'Validation fails'});
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const {provider_id,date} = req.body;
+    const { provider_id, date } = req.body;
     const isProvider = await User.findOne({
-      where: {id: provider_id, provider: true}
-    })
+      where: { id: provider_id, provider: true },
+    });
 
     /**
      * Check se é um provider
      */
-    if(!isProvider){
-      return res.status(401).json({error: 'You can only create appointments with provides'})
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with provides' });
     }
 
     /**
@@ -33,8 +34,8 @@ class AppointmentController {
      */
     const hourStart = startOfHour(parseISO(date));
 
-    if(isBefore(hourStart, new Date())){
-      return res.status(400).json({error: 'Past dates are not permitted'});
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
     }
 
     /**
@@ -44,26 +45,31 @@ class AppointmentController {
       where: {
         provider_id,
         canceled_at: null,
-        date: hourStart
-      }
+        date: hourStart,
+      },
     });
 
-    if(checkAvailability){
-      return res.status(400).json({error: 'Appointment date is not available'});
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date: hourStart
-    })
+      date: hourStart,
+    });
     return res.json(appointment);
   }
 
-  async index(req,res){
+  async index(req, res) {
+    const { page = 1 } = req.body;
     const appointment = await Appointment.findOne({
-      where: {user_id: req.userId, canceled_at: null},
+      where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id','date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
@@ -73,15 +79,14 @@ class AppointmentController {
             {
               model: File,
               as: 'avatar',
-              attributes: ['id','path','url'],
-            }
-          ]
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
         },
-       ],
-    })
+      ],
+    });
     return res.json(appointment);
   }
-
 }
 
 export default new AppointmentController();
