4868a87d936d29efe019d734332e25c8908e74e6
marcio <marcio.ferreira@cialne.com.br>
2020-10-27 13:02:29 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  segure: false,
+  auth: {
+    user: 'abd1b78c90993f',
+    pass: '50ae000c1d35f0',
+  },
+  default: {
+    from: 'Marcio tadeu <noreply@mt.com>',
+  },
+};
